3ca19045f72372ec06ef99eb1f8a8b4f33071e63
Md Mahbub Tanmay <129056181+MahbubTanmay@users.noreply.github.com>
2025-05-25 16:41:36 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ int main(){
     str = to_string(c);
     cout << "Double to String: " << str << endl;
 
-    str = string(1,c);  //This is important. For other : to_string(variable_name_here) , for char string(repeat_count , char_variable_name)
+    str = string(1,d);  //This is important. For other : to_string(variable_name_here) , for char string(repeat_count , char_variable_name)
     // string(1,'A')   output: A
     // string(2,'A')   output: AA
      // string(3,'A')   output: AAA
